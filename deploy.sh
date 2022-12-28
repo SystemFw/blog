260aea8c793f4eeacee7854e642424c2d39e05f9
Fabio Labella <fabio.labella2@gmail.com>
2022-12-28 18:23:53 +0000

    Update deploy to use new branch names

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ cabal run site clean
 cabal run site build
 
 cd deploy
-git checkout master
+git checkout main
 git pull
 
 cp -a ../_site/. .
 
 git add -A
 git commit -m "Publish."
-git push origin master:master
+git push origin main:main
 
 cd ..
